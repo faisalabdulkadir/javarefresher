c38e3b7d00b6d689f59f6fd2c9df991aecf2b382
Faisal <wizardfaisal@gmail.com>
2018-08-03 00:47:18 +0100

    New updates on objects

--- Puppy.java
+++ Puppy.java
@@ -1,10 +1,32 @@
 public class Puppy{
+  int puppyAge;
+
   public Puppy(String name){
     //constructor with a single argument, name
     System.out.println("The name Passed is: "+ name);
   }
+  public void setAge(int age){
+    /*set the age of puppy*/
+    puppyAge = age;
+  }
+
+  public int getAge(){
+    System.out.println("the age is: "+puppyAge);
+    return puppyAge;
+  }
+
   public static void main (String args[]){
     //create an object myPuppy
     Puppy myPuppy = new Puppy("Tommy");
+
+    //To set the age of puppy
+    myPuppy.setAge(4);
+
+    // To obtain the age of puppy
+    myPuppy.getAge();
+
+    //Accessing instatnce variables
+    System.out.println("The value of the instance variable is: " + myPuppy.puppyAge);
+
   }
 }
