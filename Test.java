2ccf57588432f9875f36e03730c6ff104aaa5abb
Faisal <wizardfaisal@gmail.com>
2018-08-08 01:01:57 +0100

    variables

---
+++ Test.java
@@ -0,0 +1,12 @@
+public class Test{
+  public void pupAge(){
+    int age = 0;
+    age = age + 7;
+    Stystem.out.println("The puppy's age is: " + age);
+  }
+
+  public static void main(String args[]){
+    Test test = new Test;
+    test.pupAge();
+  }
+}
